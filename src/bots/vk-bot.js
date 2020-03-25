c4e3a5ac3d7f952da00a800f148932cad848e748
Maxim Khokhryakov <khokm@ya.ru>
2020-03-26 01:20:36 +0500

    Upgrade to easyvk 2.6.0

--- src/bots/vk-bot.js
+++ src/bots/vk-bot.js
@@ -1,21 +1,21 @@
 const { Bot } = require("./bot");
 const easyvk = require("easyvk");
+const fs = require("fs");
+const path = require("path");
 
 class VkBot extends Bot {
   async showKeyboard(peer_id, message, options) {
     if (options[0].constructor !== Array) options = [options];
 
     const buttons = options.map(opts =>
-      opts.map(opt => {
-        return {
-          action: {
-            type: "text",
-            // "payload": "{\"button\": \"1\"}",
-            label: opt.label || opt
-          },
-          color: opt.color || "default"
-        };
-      })
+      opts.map(opt => ({
+        action: {
+          type: "text",
+          // "payload": "{\"button\": \"1\"}",
+          label: opt.label || opt
+        },
+        color: opt.color || "default"
+      }))
     );
 
     const keyboard = JSON.stringify({ one_time: true, buttons });
@@ -23,7 +23,8 @@ class VkBot extends Bot {
     await this.customCommand("messages.send", {
       peer_id,
       message,
-      keyboard
+      keyboard,
+      random_id: easyvk.randomId()
     });
   }
 
@@ -33,7 +34,8 @@ class VkBot extends Bot {
     await this.customCommand("messages.send", {
       peer_id,
       message,
-      attachment
+      attachment,
+      random_id: easyvk.randomId()
     });
   }
 
@@ -43,7 +45,7 @@ class VkBot extends Bot {
       authType: easyvk.GROUP_AUTH_TYPE
     });
 
-    const { connection } = await vk.bots.longpoll.connect({
+    const connection = await vk.bots.longpoll.connect({
       forGetLongPollServer: {
         group_id
       },
@@ -52,18 +54,20 @@ class VkBot extends Bot {
       }
     });
 
-    this.customCommand = async (command, params) =>
-      (await vk.post(command, params)).vkr[0];
+    this.customCommand = async (cmd, params) => await vk.post(cmd, params);
 
     this.uploadFile = async (peer_id, filePath) => {
-      const { url } = await vk.uploader.getUploadURL(
+      const realpath = path.resolve(filePath);
+      if (!fs.existsSync(realpath))
+        throw Error(`Файл ${realpath} не существует`);
+      const url = await vk.uploader.getUploadURL(
         "docs.getMessagesUploadServer",
         { peer_id },
         false
       );
-      const { vkr } = await vk.uploader.uploadFile(url, filePath, "file", {});
-      const { id, owner_id } = await this.customCommand("docs.save", {
-        file: vkr.response.file
+      const { file } = await vk.uploader.uploadFile(url, realpath, "file", {});
+      const [{ id, owner_id }] = await this.customCommand("docs.save", {
+        file
       });
       return `doc${owner_id}_${id}`;
     };
@@ -73,8 +77,8 @@ class VkBot extends Bot {
       await vk_this.long_poll(group_id, access_token, v_api);
       console.log("Реконнект в " + new Date());
     });
-    connection.on("failure", console.error);
-    connection.on("reconnectError", console.error);
+    connection.on("failure", err => console.error(err));
+    connection.on("reconnectError", err => console.error(err));
 
     connection.on("message_new", msg => {
       let peer_id,
